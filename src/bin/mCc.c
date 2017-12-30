249656a2c82dcbad5d729a33b752b99e287e72ed
Alex Hirsch <w4rh4wk@bluephoenix.at>
2017-12-30 16:06:35 +0100

    Compiler can read from files

--- src/bin/mCc.c
+++ src/bin/mCc.c
@@ -1,21 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "mCc/ast.h"
 #include "mCc/parser.h"
 
-int main(void)
+void print_usage(const char *prg)
 {
-	FILE *in = stdin;
+	printf("usage: %s <FILE>\n\n", prg);
+	printf("  <FILE>        Input filepath or - for stdin\n");
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2) {
+		print_usage(argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	FILE *in;
+	if (strcmp("-", argv[1]) == 0) {
+		in = stdin;
+	} else {
+		in = fopen(argv[1], "r");
+		if (!in) {
+			perror("fopen");
+			return EXIT_FAILURE;
+		}
+	}
 
 	struct mC_ast_expression *expr = mC_parser_parse_file(in);
 	if (!expr) {
+		fclose(in);
 		return EXIT_FAILURE;
 	}
 
 	/* TODO */
 
 	mC_ast_delete_expression(expr);
+
+	fclose(in);
 
 	return EXIT_SUCCESS;
 }
