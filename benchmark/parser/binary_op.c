700ce09ea8ebe5b82256843d13a6224ef9da86c6
Alex Hirsch <w4rh4wk@bluephoenix.at>
2018-02-09 15:58:47 +0100

    Add 2 parser micro benchmarks

---
+++ benchmark/parser/binary_op.c
@@ -0,0 +1,23 @@
+#include <assert.h>
+#include <stdlib.h>
+
+#include "mCc/ast.h"
+#include "mCc/parser.h"
+
+int main(int argc, char *argv[])
+{
+	const long reps = argc >= 2 ? atol(argv[1]) : 100000;
+
+	const char input[] = "192 + 3.14";
+
+	for (long i = 0; i < reps; i++) {
+		struct mCc_parser_result result = mCc_parser_parse_string(input);
+
+		assert(result.status == MCC_PARSER_STATUS_OK);
+
+		mCc_ast_delete_expression(result.expression);
+	}
+
+	return EXIT_SUCCESS;
+}
+
