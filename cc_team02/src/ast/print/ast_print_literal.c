21cbe388424d08fe0ffbf23642eb2203ee30ffd5
alex-bl <alexander.blaas@student.uibk.ac.at>
2018-03-19 23:47:25 +0100

    intermediate commit print-literal

---
+++ cc_team02/src/ast/print/ast_print_literal.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+
+#include "mCc/ast/basis/ast_literal.h"
+#include "mCc/ast/print/ast_basic_printing.h"
+#include "mCc/ast/print/ast_print_literal.h"
+#include "mCc/ast/visit/ast_visit_literal.h"
+
+#define LABEL_SIZE 64
+
+const char *mCc_ast_print_binary_op(enum mCc_ast_binary_op op)
+{
+	switch (op) {
+	case MCC_AST_BINARY_OP_ADD: return "+";
+	case MCC_AST_BINARY_OP_SUB: return "-";
+	case MCC_AST_BINARY_OP_MUL: return "*";
+	case MCC_AST_BINARY_OP_DIV: return "/";
+	}
+
+	return "unknown op";
+}
+
+static void print_dot_literal_int(struct mCc_ast_literal *literal, void *data)
+{
+	assert(literal);
+	assert(data);
+
+	char label[LABEL_SIZE] = { 0 };
+	snprintf(label, sizeof(label), "%ld", literal->i_value);
+
+	FILE *out = data;
+	print_dot_node(out, literal, label);
+}
+
+static void print_dot_literal_float(struct mCc_ast_literal *literal, void *data)
+{
+	assert(literal);
+	assert(data);
+
+	char label[LABEL_SIZE] = { 0 };
+	snprintf(label, sizeof(label), "%f", literal->f_value);
+
+	FILE *out = data;
+	print_dot_node(out, literal, label);
+}
+
+
+static struct mCc_ast_visitor print_dot_visitor(FILE *out)
+{
+	assert(out);
+
+	return (struct mCc_ast_visitor){
+		.traversal = MCC_AST_VISIT_DEPTH_FIRST,
+		.order = MCC_AST_VISIT_PRE_ORDER,
+
+		.userdata = out,
+
+		.literal_int = print_dot_literal_int,
+		.literal_float = print_dot_literal_float,
+	};
+}
+
+void mCc_ast_print_dot_literal(FILE *out, struct mCc_ast_literal *literal)
+{
+	assert(out);
+	assert(literal);
+
+	print_dot_begin(out);
+
+	struct mCc_ast_visitor visitor = print_dot_visitor(out);
+	mCc_ast_visit_literal(literal, &visitor);
+
+	print_dot_end(out);
+}
