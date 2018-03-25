a723b56ccc1b93f3d261c5a41fe181f38f7e6d65
Juri Seelmann <juri.seelmann@student.uibk.ac.at>
2018-03-25 13:40:12 +0200

    Implemented ast_identifier

--- cc_team02/src/ast/basis/ast_identifier.c
+++ cc_team02/src/ast/basis/ast_identifier.c
@@ -1,14 +1,21 @@
 #include <stddef.h>
 
-#include "mCc/ast/basis/ast_function.h"
+#include "mCc/ast/basis/ast_identifier.h"
 
 struct mCc_ast_identifier *mCc_ast_new_identifier(const char *name)
 {
-	/* TODO */
-	return NULL;
+	assert(name);
+
+	struct mCc_ast_identifier *expr = malloc(sizeof(*expr));
+	if (!expr) {
+		return NULL;
+	}
+
+	expr->identifier_name = name;
+	return expr;
 }
 
 void mCc_ast_delete_identifier(struct mCc_ast_identifier *name)
 {
-	/* TODO */
+	mCc_ast_delete_identifier(name);
 }
