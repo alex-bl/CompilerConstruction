c2508d4d9c76680e5c75af741f1e9420d65507b0
Juri Seelmann <juri.seelmann@student.uibk.ac.at>
2018-03-26 06:29:47 +0200

    implemented ast_statement if-statement

--- cc_team02/src/ast/basis/ast_statement.c
+++ cc_team02/src/ast/basis/ast_statement.c
@@ -7,8 +7,17 @@ mCc_ast_new_if_statement(struct mCc_ast_expression *condition_expr,
                          struct mCc_ast_statement *if_stmt,
                          struct mCc_ast_statement *else_stmt)
 {
-	/* TODO */
-	return NULL;
+	struct mCc_ast_statement *statement = malloc(sizeof(*statement));
+	if (!statement) {
+		return NULL;
+	}
+
+	statement->statement_type=MCC_AST_STATEMENT_IF;
+	statement->condition_expression=condition_expr;
+	statement->if_statement=if_stmt;
+	statement->else_statement=else_stmt;
+
+	return statement;
 }
 
 struct mCc_ast_statement *
