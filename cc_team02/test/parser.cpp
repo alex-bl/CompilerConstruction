d18a39e05562116a4f29a6af40c75b442cd0e5d5
Yusuf Ipek <yusuf.ipek2@gmail.com>
2018-03-28 15:37:34 +0200

    added more tests

--- cc_team02/test/parser.cpp
+++ cc_team02/test/parser.cpp
@@ -34,6 +34,187 @@ TEST(Parser, BinaryOp_1)
 	mCc_ast_delete_expression(expr);
 }
 
+TEST(Parser, BinaryOp_2){
+	const char input[] = "192 > 3.14";
+	auto result = mCc_parser_parse_string(input);
+
+
+	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
+
+	auto expr = result.expression;
+
+	// root
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_BINARY_OP, expr->type);
+	ASSERT_EQ(MCC_AST_BINARY_OP_GREATER_THAN, expr->op);
+
+	mCc_ast_delete_expression(expr);
+
+}
+
+TEST(Parser, BinaryOp_3){
+	const char input[] = "192 < 3.14";
+	auto result = mCc_parser_parse_string(input);
+
+
+	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
+
+	auto expr = result.expression;
+
+	// root
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_BINARY_OP, expr->type);
+	ASSERT_EQ(MCC_AST_BINARY_OP_LESS_THAN, expr->op);
+
+	mCc_ast_delete_expression(expr);
+
+}
+
+TEST(Parser, BinaryOp_4){
+	const char input[] = "192 >= 3.14";
+	auto result = mCc_parser_parse_string(input);
+
+
+	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
+
+	auto expr = result.expression;
+
+	// root
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_BINARY_OP, expr->type);
+	ASSERT_EQ(MCC_AST_BINARY_OP_GREATER_OR_EQUALS_THAN, expr->op);
+
+	mCc_ast_delete_expression(expr);
+
+}
+
+TEST(Parser, BinaryOp_5){
+	const char input[] = "192 <= 3.14";
+	auto result = mCc_parser_parse_string(input);
+
+
+	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
+
+	auto expr = result.expression;
+
+	// root
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_BINARY_OP, expr->type);
+	ASSERT_EQ(MCC_AST_BINARY_OP_LESS_OR_EQUALS_THAN, expr->op);
+
+	mCc_ast_delete_expression(expr);
+}
+
+TEST(Parser, BinaryOp_6){
+	const char input[] = "192 && 3.14";
+	auto result = mCc_parser_parse_string(input);
+
+
+	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
+
+	auto expr = result.expression;
+
+	// root
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_BINARY_OP, expr->type);
+	ASSERT_EQ(MCC_AST_BINARY_OP_AND, expr->op);
+
+	mCc_ast_delete_expression(expr);
+}
+
+
+TEST(Parser, BinaryOp_7){
+	const char input[] = "192 || 3.14";
+	auto result = mCc_parser_parse_string(input);
+
+
+	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
+
+	auto expr = result.expression;
+
+	// root
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_BINARY_OP, expr->type);
+	ASSERT_EQ(MCC_AST_BINARY_OP_OR, expr->op);
+
+	mCc_ast_delete_expression(expr);
+}
+
+TEST(Parser, BinaryOp_8){
+	const char input[] = "192 == 3.14";
+	auto result = mCc_parser_parse_string(input);
+
+
+	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
+
+	auto expr = result.expression;
+
+	// root
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_BINARY_OP, expr->type);
+	ASSERT_EQ(MCC_AST_BINARY_OP_EQUALS, expr->op);
+
+	mCc_ast_delete_expression(expr);
+}
+
+
+TEST(Parser, BinaryOp_9){
+	const char input[] = "192 != 3.14";
+	auto result = mCc_parser_parse_string(input);
+
+
+	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
+
+	auto expr = result.expression;
+
+	// root
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_BINARY_OP, expr->type);
+	ASSERT_EQ(MCC_AST_BINARY_OP_NOT_EQUALS, expr->op);
+
+	mCc_ast_delete_expression(expr);
+}
+
+TEST(Parser, BinaryOp_10){
+	const char input[] = "192 - 3.14";
+	auto result = mCc_parser_parse_string(input);
+
+
+	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
+
+	auto expr = result.expression;
+
+	// root
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_BINARY_OP, expr->type);
+	ASSERT_EQ(MCC_AST_BINARY_OP_SUB, expr->op);
+
+	mCc_ast_delete_expression(expr);
+}
+
+TEST(Parser, BinaryOp_11){
+	const char input[] = "192 * 3.14";
+	auto result = mCc_parser_parse_string(input);
+
+
+	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
+
+	auto expr = result.expression;
+
+	// root
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_BINARY_OP, expr->type);
+	ASSERT_EQ(MCC_AST_BINARY_OP_MUL, expr->op);
+
+	mCc_ast_delete_expression(expr);
+}
+
+TEST(Parser, BinaryOp_12){
+	const char input[] = "192 / 3.14";
+	auto result = mCc_parser_parse_string(input);
+
+
+	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
+
+	auto expr = result.expression;
+
+	// root
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_BINARY_OP, expr->type);
+	ASSERT_EQ(MCC_AST_BINARY_OP_DIV, expr->op);
+
+	mCc_ast_delete_expression(expr);
+}
+
 TEST(Parser, NestedExpression_1)
 {
 	const char input[] = "42 * (-192 + 3.14)";
@@ -93,9 +274,12 @@ TEST(Parser, Assignment_1)
 	const char input[] = "a = 4";
 	auto result = mCc_parser_parse_string(input);
 
-	ASSERT_EQ(4, result.assignment->assigned_expression->literal->i_value);
-	ASSERT_STREQ("a", result.assignment->identifier->identifier_name);
+	auto assignment = result.assignment;
+
+	ASSERT_EQ(4, assignment->assigned_expression->literal->i_value);
+	ASSERT_STREQ("a", assignment->identifier->identifier_name);
 	
+	mCc_ast_delete_assignment(assignment);
 }
 
 TEST(Parser, IfStatement_1)
@@ -109,8 +293,48 @@ TEST(Parser, IfStatement_1)
 	ASSERT_EQ(MCC_AST_LITERAL_TYPE_INT, if_statement->condition_expression->literal->type);
 	ASSERT_EQ(MCC_AST_STATEMENT_IF, if_statement->statement_type);
 	//mCc_ast_delete_statement(if_statement);
-
 }
+
+TEST(Parser, IfStatement_2)
+{
+
+	const char input[] = " if(5){int a;}else{int s;}";
+	auto result = mCc_parser_parse_string(input);
+
+	auto statement = result.statement;
+
+	ASSERT_STREQ("s", statement->else_statement->declaration->identifier->identifier_name);
+	
+	mCc_ast_delete_statement(statement);
+}
+
+TEST(Parser, WhileStatement_1){
+	const char input[] = "while(5){int a;}";
+	auto result = mCc_parser_parse_string(input);
+
+	auto statement = result.statement;
+	ASSERT_EQ(MCC_AST_STATEMENT_WHILE, statement->statement_type);
+
+	mCc_ast_delete_statement(statement);
+}
+
+TEST(Parser, ReturnStatement_1){
+	const char input[] = "return;";
+	auto result = mCc_parser_parse_string(input);
+	auto statement = result.statement;
+	ASSERT_EQ(MCC_AST_STATEMENT_RETURN, statement->statement_type);
+	mCc_ast_delete_statement(statement);
+}
+
+TEST(Parser, ReturnStatement_2){
+	const char input[] = "return 5;";
+	auto result = mCc_parser_parse_string(input);
+	auto statement = result.statement;
+	ASSERT_EQ(MCC_AST_STATEMENT_RETURN, statement->statement_type);
+	ASSERT_EQ(5, statement->return_expression->literal->i_value);
+	mCc_ast_delete_statement(statement);
+}
+
 
 TEST(Parser, Declaration_1){
 	const char input[] = "int a";
@@ -119,14 +343,8 @@ TEST(Parser, Declaration_1){
 	auto declaration = result.declaration;
 
 	ASSERT_EQ(MCC_AST_DECLARATION_PRIMITIVE, declaration->declaration_type);
+	ASSERT_STREQ("a", declaration->identifier->identifier_name);
 //	mCc_ast_delete_program(program);
 }
 
-TEST(Parser, Statement_1){
-	const char input[] = "if(){}else{int a;}";
-	auto result = mCc_parser_parse_string(input);
 
-	auto statement = result.statement;
-
-	ASSERT_EQ(MCC_AST_LITERAL_TYPE_INT, statement->else_statement->declaration->data_type);
-}
