0320660f72c199d4b945382e77179334511b93a9
Yusuf Ipek <yusuf.ipek2@gmail.com>
2018-03-28 14:33:00 +0200

    added some tests

--- cc_team02/test/parser.cpp
+++ cc_team02/test/parser.cpp
@@ -1,4 +1,5 @@
 #include "gtest/gtest.h"
+#include <iostream>
 
 #include "mCc/ast.h"
 #include "mCc/parser.h"
@@ -37,7 +38,7 @@ TEST(Parser, NestedExpression_1)
 {
 	const char input[] = "42 * (-192 + 3.14)";
 	auto result = mCc_parser_parse_string(input);
-
+	
 	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
 
 	auto expr = result.expression;
@@ -85,4 +86,47 @@ TEST(Parser, MissingClosingParenthesis_1)
 	auto result = mCc_parser_parse_string(input);
 
 	ASSERT_NE(MCC_PARSER_STATUS_OK, result.status);
+}
+
+TEST(Parser, Assignment_1)
+{
+	const char input[] = "a = 4";
+	auto result = mCc_parser_parse_string(input);
+
+	ASSERT_EQ(4, result.assignment->assigned_expression->literal->i_value);
+	ASSERT_STREQ("a", result.assignment->identifier->identifier_name);
+	
+}
+
+TEST(Parser, IfStatement_1)
+{
+
+	const char input[] = " if(5){}";
+	auto result = mCc_parser_parse_string(input);
+
+	auto if_statement = result.statement;
+
+	ASSERT_EQ(MCC_AST_LITERAL_TYPE_INT, if_statement->condition_expression->literal->type);
+	ASSERT_EQ(MCC_AST_STATEMENT_IF, if_statement->statement_type);
+	//mCc_ast_delete_statement(if_statement);
+
+}
+
+TEST(Parser, Declaration_1){
+	const char input[] = "int a";
+	
+	auto result = mCc_parser_parse_string(input);
+	auto declaration = result.declaration;
+
+	ASSERT_EQ(MCC_AST_DECLARATION_PRIMITIVE, declaration->declaration_type);
+//	mCc_ast_delete_program(program);
+}
+
+TEST(Parser, Statement_1){
+	const char input[] = "if(){}else{int a;}";
+	auto result = mCc_parser_parse_string(input);
+
+	auto statement = result.statement;
+
+	ASSERT_EQ(MCC_AST_LITERAL_TYPE_INT, statement->else_statement->declaration->data_type);
 }
