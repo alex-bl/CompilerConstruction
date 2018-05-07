52cc2379412406f1b32fff1805ce6d9d5774efdc
Juri Seelmann <juri.seelmann@student.uibk.ac.at>
2018-05-07 18:48:15 +0200

    added information about tac structure

--- cc_team02/include/mCc/ast/basis/ast_data_type.h
+++ cc_team02/include/mCc/ast/basis/ast_data_type.h
@@ -5,14 +5,24 @@
 extern "C" {
 #endif
 
-enum mCc_ast_data_type {
+/*enum mCc_ast_data_type {
 	// use void only for functions!
 	MCC_AST_DATA_TYPE_VOID,
 	MCC_AST_DATA_TYPE_INT,
 	MCC_AST_DATA_TYPE_FLOAT,
 	MCC_AST_DATA_TYPE_BOOL,
 	MCC_AST_DATA_TYPE_STRING
-};
+};*/
+
+/*
+ * Types of TAC:
+ * Assignment statement: x:= y op z
+ * Assignment statement: x:= op z
+ * copy statement: x:= y
+ * Unconditional jump: goto L
+ * Conditional jump: if x relop y goto L
+ * Procedural call: param x call p return y
+ */
 
 enum mCc_tac_operation {
 	MCC_TAC_OPARATION_JUMP,
