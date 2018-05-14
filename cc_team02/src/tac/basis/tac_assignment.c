7e8c9bca4e4f870b28ba43bdf254b8200878e400
Juri Seelmann <juri.seelmann@student.uibk.ac.at>
2018-05-14 19:53:27 +0200

    corrected tac assignment

--- cc_team02/src/tac/basis/tac_assignment.c
+++ cc_team02/src/tac/basis/tac_assignment.c
@@ -4,21 +4,18 @@
 
 #include "mCc/tac/basis/basic_tac.h"
 
+// x:= y
 void mCc_tac_assignment_primitive(struct mCc_ast_assignment *assignment,
                                   struct mCc_tac_element *previous_tac)
 {
 	assert(assignment);
 	assert(previous_tac);
 
-	struct mCc_tac_element *tac =
-	    tac_new_element(MCC_TAC_OPERATION_EMPTY,
-	                    tac_new_identifier(assignment->assigned_expression),
-	                    NULL, tac_new_identifier(assignment->identifier));
+	struct mCc_tac_element *tac = tac_new_element(
+	    MCC_TAC_OPARATION_COPY,
+	    tac_new_identifier(assignment->assigned_expression->literal->s_value),
+	    NULL, tac_new_identifier(assignment->identifier->identifier_name));
 	mCc_tac_connect_tac_entry(previous_tac, tac);
-
-	// FILE *out = data;
-	// fprintf(out, "assignment: %p - identifier: %p - expression: %p",
-	// assignment, assignment->identifier, assignment->assigned_expression);
 }
 
 void mCc_tac_assignment_array(struct mCc_ast_assignment *assignment,
@@ -27,15 +24,13 @@ void mCc_tac_assignment_array(struct mCc_ast_assignment *assignment,
 	assert(assignment);
 	assert(previous_tac);
 
-	// array_index_expression->array_identifier
-	// array_index_expression->array_index_expression
-	// array_assigned_expression
+	// y[i]:=x
 	struct mCc_tac_element *tac = tac_new_element(
 	    MCC_TAC_OPARATION_INDEXING,
-	    tac_new_identifier(
-	        assignment->array_index_expression->array_index_expression),
+	    tac_new_identifier(assignment->array_index_expression->array_identifier
+	                           ->identifier_name),
 	    tac_new_identifier(
 	        assignment->array_assigned_expression->identifier->identifier_name),
-	    tac_new_identifier(assignment->identifier));
+	    tac_new_identifier(assignment->identifier->identifier_name));
 	mCc_tac_connect_tac_entry(previous_tac, tac);
 }
