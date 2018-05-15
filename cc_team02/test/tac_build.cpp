8163b766ddba04938b79a4e7323cf567098e8d10
Juri Seelmann <juri.seelmann@student.uibk.ac.at>
2018-05-15 09:44:49 +0200

    added files for tac generation and printing tests

---
+++ cc_team02/test/tac_build.cpp
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "gtest/gtest.h"
+
+#include "mCc/ast_tac.h"
+#include "mCc_test/mCc_test_utils.h"
+#include "map.h"
+
+//============================================ TAC
+TEST(TAC, firstTest)
+{
+
+}
