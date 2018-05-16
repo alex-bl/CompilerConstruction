f83698cfe65c3afd31e4040d35fc03956ece5466
Juri Seelmann <juri.seelmann@student.uibk.ac.at>
2018-05-16 22:28:28 +0200

    added more test to get to 200 fixed bug in deleting tac

--- cc_team02/test/tac_build.cpp
+++ cc_team02/test/tac_build.cpp
@@ -3,6 +3,7 @@
 
 #include "basic_tac.h"
 #include "mCc/tac.h"
+#include "tac_expression.h"
 
 #include "gtest/gtest.h"
 
@@ -21,6 +22,8 @@ TEST(TAC, BasicTAC)
 	// ASSERT_TRUE(true);
 	ASSERT_EQ(tac->tac_operation, MCC_TAC_OPARATION_EMPTY);
 	ASSERT_EQ(tac->tac_argument1->name, name);
+
+	mCc_tac_delete(tac);
 }
 
 TEST(TAC, ConnectionBetweenTACs)
@@ -38,8 +41,48 @@ TEST(TAC, ConnectionBetweenTACs)
 
 	ASSERT_EQ(tac->tac_next_element, tac2);
 	ASSERT_EQ(tac->tac_next_element->tac_result, tac2->tac_result);
+
+	mCc_tac_delete(tac);
 }
 
-TEST(TAC, TACTest) {
+TEST(TAC, TACFloatLiteralExpression)
+{
+	char name[] = "ident_name";
+	struct mCc_tac_identifier *ident1 = tac_new_identifier(name);
 
+	struct mCc_tac_element *previous_tac =
+	    tac_new_element(MCC_TAC_OPARATION_EMPTY, ident1, ident1, ident1);
+
+	struct mCc_ast_literal *literal=mCc_ast_new_literal_float(12.3);
+
+	struct mCc_ast_expression *expression=mCc_ast_new_expression_literal(literal);
+
+	struct mCc_tac_element *tac =
+	    mCc_tac_expression_literal(expression, previous_tac);
+
+	ASSERT_EQ(tac->tac_operation, MCC_TAC_OPARATION_EMPTY);
+	ASSERT_EQ(tac->tac_argument1->name, (char *)&expression->literal->f_value);
+
+	mCc_tac_delete(tac);
+}
+
+TEST(TAC, TACIntLiteralExpression)
+{
+	char name[] = "ident_name";
+	struct mCc_tac_identifier *ident1 = tac_new_identifier(name);
+
+	struct mCc_tac_element *previous_tac =
+	    tac_new_element(MCC_TAC_OPARATION_EMPTY, ident1, ident1, ident1);
+
+	struct mCc_ast_literal *literal=mCc_ast_new_literal_int(4);
+
+	struct mCc_ast_expression *expression=mCc_ast_new_expression_literal(literal);
+
+	struct mCc_tac_element *tac =
+	    mCc_tac_expression_literal(expression, previous_tac);
+
+	ASSERT_EQ(tac->tac_operation, MCC_TAC_OPARATION_EMPTY);
+	ASSERT_EQ(tac->tac_argument1->name, (char *)&expression->literal->i_value);
+
+	mCc_tac_delete(tac);
 }
