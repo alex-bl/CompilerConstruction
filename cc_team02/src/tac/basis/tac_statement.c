9a16be35dba0edc9e680ae3beb54b1715149f0c3
Juri Seelmann <juri.seelmann@student.uibk.ac.at>
2018-05-16 14:08:33 +0200

    continued working on statements (if-statement)

--- cc_team02/src/tac/basis/tac_statement.c
+++ cc_team02/src/tac/basis/tac_statement.c
@@ -1,10 +1,36 @@
 #include "tac_statement.h"
+#include "ast_expression.h"
 #include "tac_expression.h"
-#include "tac_declaration.h"
 
 #include <assert.h>
 
 #include "basic_tac.h"
+
+// not really used - till now?
+char *helper_type_to_char(enum mCc_ast_expression_type type)
+{
+	/*	MCC_AST_EXPRESSION_TYPE_LITERAL,
+	MCC_AST_EXPRESSION_TYPE_BINARY_OP,
+	MCC_AST_EXPRESSION_TYPE_PARENTH,
+	MCC_AST_EXPRESSION_TYPE_IDENTIFIER,
+	MCC_AST_EXPRESSION_TYPE_IDENTIFIER_ARRAY,
+	MCC_AST_EXPRESSION_TYPE_CALL_EXPR,
+	MCC_AST_EXPRESSION_TYPE_UNARY_OP */
+
+	char *return_char;
+	switch (type) {
+	case MCC_AST_EXPRESSION_TYPE_LITERAL: return_char = "void"; break;
+	case MCC_AST_EXPRESSION_TYPE_BINARY_OP: return_char = "int"; break;
+	case MCC_AST_EXPRESSION_TYPE_PARENTH: return_char = "float"; break;
+	case MCC_AST_EXPRESSION_TYPE_IDENTIFIER: return_char = "bool"; break;
+	case MCC_AST_EXPRESSION_TYPE_IDENTIFIER_ARRAY:
+		return_char = "string";
+		break;
+	case MCC_AST_EXPRESSION_TYPE_CALL_EXPR: return_char = "inconsistent"; break;
+	case MCC_AST_EXPRESSION_TYPE_UNARY_OP: return_char = "incompatible"; break;
+	}
+	return return_char;
+}
 
 // TODO recursive structure
 struct mCc_tac_element *
@@ -14,34 +40,53 @@ mCc_tac_statement_if(struct mCc_ast_statement *statement,
 	assert(statement);
 	assert(previous_tac);
 
-	//helper_data_type_to_char(statement->condition_expression)
-	/*struct mCc_tac_element *tac_expression;
-	char *return_char;
-		switch (statement->condition_expression) {
-		case MCC_AST_DATA_TYPE_VOID: tac_expression =
-			    mCc_tac_expression(statement->condition_expression, previous_tac);; break;
-		case MCC_AST_DATA_TYPE_INT: tac_expression =
-			    mCc_tac_expression(statement->condition_expression, previous_tac);; break;
-		case MCC_AST_DATA_TYPE_FLOAT: tac_expression =
-			    mCc_tac_expression(statement->condition_expression, previous_tac);; break;
-		case MCC_AST_DATA_TYPE_BOOL: tac_expression =
-			    mCc_tac_expression(statement->condition_expression, previous_tac);; break;
-		case MCC_AST_DATA_TYPE_STRING: tac_expression =
-			    mCc_tac_expression(statement->condition_expression, previous_tac);; break;
-		case MCC_AST_DATA_TYPE_INCONSISTENT: rtac_expression =
-			    mCc_tac_expression(statement->condition_expression, previous_tac);; break;
-		case MCC_AST_DATA_TYPE_INCOMPATIBLE: tac_expression =
-			    mCc_tac_expression(statement->condition_expression, previous_tac); break;
-		case MCC_AST_DATA_TYPE_UNKNOWN: tac_expression =
-			    mCc_tac_expression(statement->condition_expression, previous_tac); break;
-		}*/
+	struct mCc_tac_element *tac_statement;
 
-	struct mCc_tac_element *tac =
-	    tac_new_element(
-	        MCC_TAC_OPARATION_EMPTY,
-	        tac_new_identifier(statement->if_statement->condition_expression
-	                               ->identifier->identifier_name),
-	        NULL, NULL);
+	switch (statement->condition_expression->type) {
+	case MCC_AST_EXPRESSION_TYPE_LITERAL:
+		tac_statement = mCc_tac_expression_literal(
+		    statement->condition_expression, previous_tac);
+		break;
+	case MCC_AST_EXPRESSION_TYPE_BINARY_OP:
+		tac_statement = mCc_tac_expression_binary_op(
+		    statement->condition_expression, previous_tac);
+		break;
+	case MCC_AST_EXPRESSION_TYPE_PARENTH:
+		tac_statement = mCc_tac_expression_parenth(
+		    statement->condition_expression, previous_tac);
+		break;
+	case MCC_AST_EXPRESSION_TYPE_IDENTIFIER:
+		tac_statement = mCc_tac_expression_identifier(
+		    statement->condition_expression, previous_tac);
+		break;
+	case MCC_AST_EXPRESSION_TYPE_IDENTIFIER_ARRAY:
+		tac_statement = mCc_tac_expression_identifier_array(
+		    statement->condition_expression, previous_tac);
+		break;
+	case MCC_AST_EXPRESSION_TYPE_CALL_EXPR:
+		tac_statement = mCc_tac_expression_function_call(
+		    statement->condition_expression, previous_tac);
+		break;
+	case MCC_AST_EXPRESSION_TYPE_UNARY_OP:
+		tac_statement = mCc_tac_expression_unary_op(
+		    statement->condition_expression, previous_tac);
+		break;
+	}
+	mCc_tac_connect_tac_entry(previous_tac, tac_statement);
+
+	struct mCc_tac_element *tac = tac_new_element(
+			MCC_TAC_OPARATION_CONDITIONAL_JUMP,
+		   tac_statement->tac_result,
+		    NULL/*if statement*/, NULL);
+
+	//statement->if_statement;
+	//statement->else_statement;
+
+	struct mCc_tac_element *tac = tac_new_element(
+	    MCC_TAC_OPARATION_EMPTY,
+	    tac_new_identifier(statement->if_statement->condition_expression
+	                           ->identifier->identifier_name),
+	    NULL, NULL);
 	mCc_tac_connect_tac_entry(previous_tac, tac);
 	return tac;
 }
