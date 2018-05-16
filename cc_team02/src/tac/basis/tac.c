b14a29005a0d39ef5d4e374cd875eca2219a9be1
Juri Seelmann <juri.seelmann@student.uibk.ac.at>
2018-05-16 12:54:20 +0200

    excluded the visitor form tac

--- cc_team02/src/tac/basis/tac.c
+++ cc_team02/src/tac/basis/tac.c
@@ -7,90 +7,91 @@
 #include "mCc/ast_visit.h"
 
 //"global" visitor needed
-static struct mCc_ast_visitor tac_visitor(struct mCc_tac_element *tac)
+/*static struct mCc_ast_visitor tac_visitor(struct mCc_tac_element *tac)
 {
-	assert(tac);
+    assert(tac);
 
-	return (struct mCc_ast_visitor){
+    return (struct mCc_ast_visitor){
 
-		.traversal = MCC_AST_VISIT_DEPTH_FIRST,
-		.order = MCC_AST_VISIT_POST_ORDER,
-		.userdata = tac,
+        .traversal = MCC_AST_VISIT_DEPTH_FIRST,
+        .order = MCC_AST_VISIT_POST_ORDER,
+        .userdata = tac,
 
-		//.expression
-		.expression_literal = mCc_tac_expression_literal,
-		.expression_binary_op = mCc_tac_expression_binary_op,
-		.expression_parenth = mCc_tac_expression_parenth,
+        //.expression
+        .expression_literal = mCc_tac_expression_literal,
+        .expression_binary_op = mCc_tac_expression_binary_op,
+        .expression_parenth = mCc_tac_expression_parenth,
 
-		.expression_function_call = mCc_tac_expression_function_call,
-		.expression_identifier = mCc_tac_expression_identifier,
-		.expression_array_identifier = mCc_tac_expression_identifier_array,
-		.expression_unary_op = mCc_tac_expression_unary_op,
+        .expression_function_call = mCc_tac_expression_function_call,
+        .expression_identifier = mCc_tac_expression_identifier,
+        .expression_array_identifier = mCc_tac_expression_identifier_array,
+        .expression_unary_op = mCc_tac_expression_unary_op,
 
-		//.literal
-		.literal_int = mCc_tac_literal_int,
-		.literal_float = mCc_tac_literal_float,
-		.literal_bool = mCc_tac_literal_bool,
-		.literal_string = mCc_tac_literal_string,
+        //.literal
+        .literal_int = mCc_tac_literal_int,
+        .literal_float = mCc_tac_literal_float,
+        .literal_bool = mCc_tac_literal_bool,
+        .literal_string = mCc_tac_literal_string,
 
-		//.assignment
-		.assignment_primitive = mCc_tac_assignment_primitive,
-		.assignment_array = mCc_tac_assignment_array,
+        //.assignment
+        .assignment_primitive = mCc_tac_assignment_primitive,
+        .assignment_array = mCc_tac_assignment_array,
 
-		//.declaration
-		.declaration_primitive = mCc_tac_declaration_primitive,
-		.declaration_array = mCc_tac_declaration_array,
+        //.declaration
+        .declaration_primitive = mCc_tac_declaration_primitive,
+        .declaration_array = mCc_tac_declaration_array,
 
-		.function_call = mCc_tac_function_call,
-		.function_def = mCc_tac_function_def,
+        .function_call = mCc_tac_function_call,
+        .function_def = mCc_tac_function_def,
 
-		// identifier
-		.identifier = mCc_tac_identifier,
+        // identifier
+        .identifier = mCc_tac_identifier,
 
-		// program
-		.program = mCc_tac_program,
+        // program
+        .program = mCc_tac_program,
 
-		// statement
-		.statement_if = mCc_tac_statement_if,
-		.statement_while = mCc_tac_statement_while,
-		.statement_return = mCc_tac_statement_return,
-		.statement_declaration = mCc_tac_statement_declaration,
-		.statement_assignment = mCc_tac_statement_assignment,
-		.statement_expression = mCc_tac_statement_expression,
+        // statement
+        .statement_if = mCc_tac_statement_if,
+        .statement_while = mCc_tac_statement_while,
+        .statement_return = mCc_tac_statement_return,
+        .statement_declaration = mCc_tac_statement_declaration,
+        .statement_assignment = mCc_tac_statement_assignment,
+        .statement_expression = mCc_tac_statement_expression,
 
-		// not needed for print
-		.statement = NULL,
-		.assignment = NULL,
-		.declaration = NULL,
-		.literal = NULL,
-		.expression = NULL,
+        // not needed for print
+        .statement = NULL,
+        .assignment = NULL,
+        .declaration = NULL,
+        .literal = NULL,
+        .expression = NULL,
 
-		// scope enter/leave hooks also not needed here
-		.function_def_enter_scope = NULL,
-		.function_def_leave_scope = NULL,
+        // scope enter/leave hooks also not needed here
+        .function_def_enter_scope = NULL,
+        .function_def_leave_scope = NULL,
 
-		.statement_if_enter_scope = NULL,
-		.statement_if_leave_scope = NULL,
-		.statement_while_enter_scope = NULL,
-		.statement_while_leave_scope = NULL
-	};
-}
+        .statement_if_enter_scope = NULL,
+        .statement_if_leave_scope = NULL,
+        .statement_while_enter_scope = NULL,
+        .statement_while_leave_scope = NULL
+    };
+}*/
 
 /*
  * should be the "top"
  * is "global"
  */
 struct mCc_tac_element *mCc_tac_start_program(struct mCc_tac_element *tac,
-                                             struct mCc_ast_program *program)
+                                              struct mCc_ast_program *program)
 {
 	assert(tac);
 	assert(program);
 
-	struct mCc_tac_element *empty_tac =
-	    tac_new_element(MCC_TAC_OPARATION_EMPTY, NULL, NULL, NULL);
+	//struct mCc_tac_element *empty_tac =
+	 //   tac_new_element(MCC_TAC_OPARATION_EMPTY, NULL, NULL, NULL);
 
-	struct mCc_ast_visitor visitor = tac_visitor(empty_tac);
-	mCc_ast_visit_program(program, &visitor);
+	//struct mCc_ast_visitor visitor = tac_visitor(empty_tac);
+	// mCc_ast_visit_program(program, &visitor);
+	mCc_tac_program(program, tac);
 
 	return tac;
 }
