e5b9e18eaa4057e3aec57be316c6197933d46da4
Juri Seelmann <juri.seelmann@student.uibk.ac.at>
2018-05-16 11:24:29 +0200

    added recursive call of statements in tac_function

--- cc_team02/src/tac/basis/tac_function.c
+++ cc_team02/src/tac/basis/tac_function.c
@@ -1,5 +1,6 @@
 #include "tac_function.h"
 #include "tac_declaration.h"
+#include "tac_statement.h"
 
 #include <assert.h>
 #include <stdlib.h>
@@ -38,15 +39,45 @@ mCc_tac_function_def(struct mCc_ast_function_def *def,
 	struct mCc_ast_statement *statement = def->first_statement;
 	while (statement != NULL) {
 		/* Statement-Types:
-			MCC_AST_STATEMENT_IF,
-			MCC_AST_STATEMENT_WHILE,
-			MCC_AST_STATEMENT_RETURN,
+		    MCC_AST_STATEMENT_IF,
+		    MCC_AST_STATEMENT_WHILE,
+		    MCC_AST_STATEMENT_RETURN,
 
-			MCC_AST_STATEMENT_DECLARATION,
-			MCC_AST_STATEMENT_ASSIGNMENT,
-			MCC_AST_STATEMENT_EXPRESSION
+		    MCC_AST_STATEMENT_DECLARATION,
+		    MCC_AST_STATEMENT_ASSIGNMENT,
+		    MCC_AST_STATEMENT_EXPRESSION
 		 */
-
+		struct mCc_tac_element *statement_tac;
+		switch (statement->statement_type) {
+		case MCC_AST_STATEMENT_IF:
+			statement_tac = mCc_tac_statement_if(statement, previous_tac);
+			break;
+		case MCC_AST_STATEMENT_WHILE:
+			statement_tac = mCc_tac_statement_while(statement, previous_tac);
+			break;
+		case MCC_AST_STATEMENT_RETURN:
+			statement_tac = mCc_tac_statement_return(statement, previous_tac);
+			break;
+		case MCC_AST_STATEMENT_DECLARATION:
+			statement_tac =
+			    mCc_tac_statement_declaration(statement, previous_tac);
+			break;
+		case MCC_AST_STATEMENT_ASSIGNMENT:
+			statement_tac =
+			    mCc_tac_statement_assignment(statement, previous_tac);
+			break;
+		case MCC_AST_STATEMENT_EXPRESSION:
+			statement_tac =
+			    mCc_tac_statement_expression(statement, previous_tac);
+			break;
+		}
+		mCc_tac_connect_tac_entry(previous_tac, statement_tac);
+		//Has a statement to be in the tac table?
+		struct mCc_tac_element *tac = tac_new_element(
+		    MCC_TAC_OPARATION_LABLE, statement_tac->tac_result, NULL,
+		    tac_new_identifier(def->identifier->identifier_name));
+		mCc_tac_connect_tac_entry(statement_tac, tac);
+		previous_tac = tac;
 		statement = statement->next_statement;
 	}
 
