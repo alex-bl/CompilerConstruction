712c666b59332e09265f23ff88ab669b0f84e939
alex-bl <19743442+alex-bl@users.noreply.github.com>
2018-05-27 11:47:34 +0200

    test-infrastructure for assembly-functionality added

---
+++ cc_team02/test/assembly.cpp
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "gtest/gtest.h"
+
+#include "mCc/ast.h"
+#include "mCc/parser.h"
+#include "mCc_test/mCc_test_utils.h"
+#include "parser_helper.h"
+
+
+TEST(AssemblyGeneration, SimpleIntegerAdd)
+{
+	/*TODO*/
+}
