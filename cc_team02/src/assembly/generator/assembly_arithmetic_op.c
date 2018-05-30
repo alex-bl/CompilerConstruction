be56c1e9f09d84e6d88c9eb66c37afcfc2df111c
alex-bl <19743442+alex-bl@users.noreply.github.com>
2018-05-30 12:03:31 +0200

    intermediate commit assembly-add

--- cc_team02/src/assembly/generator/assembly_arithmetic_op.c
+++ cc_team02/src/assembly/generator/assembly_arithmetic_op.c
@@ -3,14 +3,28 @@
 
 #include <assert.h>
 
+//TODO: Überprüfe, ob %eax auch richtig verwendet wurde!!!
+
+void handle_tac_identifier(FILE *out, struct mCc_tac_identifier *identifier,
+                           const char *target_reg)
+{
+	if (identifier->type == MCC_IDENTIFIER_TAC_TYPE_INTEGER) {
+		fprintf("\taddl $%ld,%s", identifier->i_val, target_reg);
+	} else {
+		fprintf(out, "\tmovl -%d(%%ebp), %s", identifier->stack_offset,
+		        target_reg);
+	}
+}
+
 void mCc_assembly_generate_add(FILE *out, struct mCc_tac_element *tac_elem)
 {
 	assert(out);
 	assert(tac_elem);
-	const char *operant_1 = tac_elem->tac_argument1->name;
-	const char *operant_2 = tac_elem->tac_argument2->name;
 
-	assembly_add_integers(out, operant_1, operant_2);
+	handle_tac_identifier(out,tac_elem->tac_argument1,"%eax");
+	handle_tac_identifier(out,tac_elem->tac_argument2,"%ecx");
+	fprintf(out,"\taddl %%ecx,%%eax");
+	fprintf(out,"\tmovl %%eax,-%d(%%ebp)",tac_elem->tac_result->stack_offset);
 }
 
 void mCc_assembly_generate_sub(FILE *out, struct mCc_tac_element *tac_elem)
