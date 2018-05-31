8132489e705fde6fa62c4546558c9f255064f800
alex-bl <19743442+alex-bl@users.noreply.github.com>
2018-05-31 22:47:13 +0200

    continued with assembler-basics

--- cc_team02/src/assembly/generator/assembly_general.c
+++ cc_team02/src/assembly/generator/assembly_general.c
@@ -10,6 +10,7 @@ void mCc_assembly_load_int_from_stack(FILE *out, int stack_offset,
 void mCc_assembly_load_float_from_stack(FILE *out, int stack_offset,
                                         const char *dest)
 {
+	fprintf(out, "\tfstps -%d(%s)", stack_offset, dest);
 }
 
 void mCc_assembly_load_int_val_to_register(FILE *out, int int_val,
@@ -21,6 +22,7 @@ void mCc_assembly_load_int_val_to_register(FILE *out, int int_val,
 void mCc_assembly_load_float_val_to_register(FILE *out, int int_val,
                                              const char *dest)
 {
+	//?
 }
 
 void mCc_assembly_allocate_int_on_stack(FILE *out, size_t space)
@@ -28,7 +30,9 @@ void mCc_assembly_allocate_int_on_stack(FILE *out, size_t space)
 	fprintf(out, "\tsubl $%ld, %%esp", space);
 }
 
-void mCc_assembly_allocate_float_on_stack(FILE *out, size_t space) {}
+void mCc_assembly_allocate_float_on_stack(FILE *out, size_t space) {
+	fprintf(out, "\tsubl $%ld, %%esp", space);
+}
 
 // stack_offset needs to be negative?
 void mCc_assembly_push_int_on_stack(FILE *out, int stack_offset,
@@ -40,4 +44,5 @@ void mCc_assembly_push_int_on_stack(FILE *out, int stack_offset,
 void mCc_assembly_push_float_on_stack(FILE *out, int stack_offset,
                                       const char *dest)
 {
+	fprintf(out,"\tflds	%d(%ebp)",stack_offset);
 }
