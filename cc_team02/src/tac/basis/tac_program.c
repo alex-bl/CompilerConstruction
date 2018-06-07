e1712554aa19ca3a32d29edcd465c8ea0a8b6415
Juri Seelmann <juri.seelmann@student.uibk.ac.at>
2018-06-07 19:02:31 +0200

    writing list of all function defs at the beginning of the tac table followed by a blank line to separate it

--- cc_team02/src/tac/basis/tac_program.c
+++ cc_team02/src/tac/basis/tac_program.c
@@ -11,13 +11,32 @@ struct mCc_tac_element *mCc_tac_program(struct mCc_ast_program *program,
 	assert(program);
 	assert(previous_tac);
 
-	struct mCc_ast_function_def *function_def = program->first_function_def;
-	/* Skip all build-ins */
-	/*while (function_def != NULL && function_def->build_in_stub) {
-	    function_def = function_def->next_function_def;
-	}*/
+	// writing a list of all function at the beginning of the tac table:
+	struct mCc_ast_function_def *function_def_list =
+	    program->first_function_def;
+	while (function_def_list != NULL) {
+		/* Skip all build-ins */
+		if (!function_def_list->build_in_stub) {
+			struct mCc_tac_element *tac_element = tac_new_element(
+			    MCC_TAC_OPARATION_FUNCTION_DEF, NULL, NULL,
+			    tac_new_identifier(
+			        function_def_list->identifier->identifier_name),
+			    MCC_TAC_TYPE_NO_TYPE, 0);
+			mCc_tac_connect_tac_entry(previous_tac, tac_element);
+			previous_tac = tac_element;
+		}
+		function_def_list = function_def_list->next_function_def;
+	}
 
-	// assert(function_def);
+	// one empty element in the tac table to separate the function list from the
+	// following
+	struct mCc_tac_element *tac_element = tac_new_element(
+	    MCC_TAC_OPARATION_EMPTY, NULL, NULL, NULL, MCC_TAC_TYPE_NO_TYPE, 0);
+	mCc_tac_connect_tac_entry(previous_tac, tac_element);
+	previous_tac = tac_element;
+
+	// looping throw all function defs and building up the tac table
+	struct mCc_ast_function_def *function_def = program->first_function_def;
 
 	while (function_def != NULL) {
 		/* Skip all build-ins */
@@ -30,10 +49,5 @@ struct mCc_tac_element *mCc_tac_program(struct mCc_ast_program *program,
 		function_def = function_def->next_function_def;
 	}
 
-	// no tac element necessary for program
-	// struct mCc_tac_element *tac = tac_new_element(
-	//    MCC_TAC_OPARATION_RETURN,
-	//    tac_new_identifier(program->first_function_def), NULL, NULL);
-	// mCc_tac_connect_tac_entry(previous_tac, tac);
 	return previous_tac;
 }
