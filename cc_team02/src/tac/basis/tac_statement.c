c747885d09c20d27c21f2aa66be03cbcc8561981
Juri Seelmann <juri.seelmann@student.uibk.ac.at>
2018-06-10 18:01:16 +0200

    continued implementing the new tac oparations

--- cc_team02/src/tac/basis/tac_statement.c
+++ cc_team02/src/tac/basis/tac_statement.c
@@ -67,11 +67,40 @@ mCc_tac_statement_if(struct mCc_ast_statement *statement,
 	struct mCc_tac_element *tac_condition = helper_get_tac_of_expression(
 	    statement->condition_expression, tac_lable_before_if);
 
+	enum mCc_tac_operation if_jump;
+	switch (tac_condition->tac_operation) {
+	case MCC_TAC_OPARATION_EQUALS_INT:
+	case MCC_TAC_OPARATION_EQUALS_FLOAT:
+	case MCC_TAC_OPARATION_EQUALS_BOOL:
+		if_jump = MCC_TAC_OPARATION_JUMP_EQUALS;
+		break;
+	case MCC_TAC_OPARATION_NOT_EQUEALS_INT:
+	case MCC_TAC_OPARATION_NOT_EQUALS_FLOAT:
+	case MCC_TAC_OPARATION_NOT_EQUALS_BOOL:
+		if_jump = MCC_TAC_OPARATION_JUMP_NOT_EQUALS;
+		break;
+	case MCC_TAC_OPARATION_GREATER_INT:
+	case MCC_TAC_OPARATION_GREATER_FLOAT:
+		if_jump = MCC_TAC_OPARATION_JUMP_GREATER;
+		break;
+	case MCC_TAC_OPARATION_LESS_INT:
+	case MCC_TAC_OPARATION_LESS_FLOAT:
+		if_jump = MCC_TAC_OPARATION_JUMP_LESS;
+		break;
+	case MCC_TAC_OPARATION_GREATER_EQUALS_INT:
+	case MCC_TAC_OPARATION_GREATER_EQUALS_FLOAT:
+		if_jump = MCC_TAC_OPARATION_JUMP_GRATER_EQUALS;
+		break;
+	case MCC_TAC_OPARATION_LESS_EQUALS_INT:
+	case MCC_TAC_OPARATION_LESS_EQUALS_FLOAT:
+		if_jump = MCC_TAC_OPARATION_JUMP_LESS_EQUALS;
+		break;
+	default: if_jump = MCC_TAC_OPARATION_JUMP_FALSE; break;
+	}
 	// jump to else, if condition is false
 	struct mCc_tac_element *tac_if_condition = tac_new_element(
-	    MCC_TAC_OPARATION_JUMP_FALSE,
-	    mCc_tac_create_from_tac_identifier(tac_condition->tac_result), NULL,
-	    mCc_tac_create_from_tac_identifier(lable_before_else),
+	    if_jump, mCc_tac_create_from_tac_identifier(tac_condition->tac_result),
+	    NULL, mCc_tac_create_from_tac_identifier(lable_before_else),
 	    MCC_TAC_TYPE_NO_TYPE, 0);
 	mCc_tac_connect_tac_entry(previous_tac, tac_if_condition);
 
@@ -106,8 +135,8 @@ mCc_tac_statement_if(struct mCc_ast_statement *statement,
 	}
 
 	struct mCc_tac_element *tac_lable_after_else =
-	    tac_new_element(MCC_TAC_OPARATION_LABEL_IF, NULL, NULL, lable_after_else,
-	                    MCC_TAC_TYPE_NO_TYPE, 0);
+	    tac_new_element(MCC_TAC_OPARATION_LABEL_IF, NULL, NULL,
+	                    lable_after_else, MCC_TAC_TYPE_NO_TYPE, 0);
 	mCc_tac_connect_tac_entry(tac_else_statement, tac_lable_after_else);
 
 	return tac_lable_after_else;
