2b86f70b823a5d4a3f02dda192ea78494ae86e0c
Juri Seelmann <juri.seelmann@student.uibk.ac.at>
2018-06-20 12:08:28 +0200

    created function stubs for tac cfg

--- cc_team02/include/mCc/tac_cfg.h
+++ cc_team02/include/mCc/tac_cfg.h
@@ -9,7 +9,26 @@
 extern "C" {
 #endif
 
-void mCc_tac_cfg_generate(struct mCc_tac_element *tac);
+struct mCc_tac_cfg_element {
+	struct mCc_tac_element *tac_element;
+	struct mCc_tac_cfg_element *next_cfg_element_left;
+	struct mCc_tac_cfg_element *next_cfg_element_right;
+	void *meta_info;
+};
+
+struct mCc_tac_cfg_element *
+mCc_tac_cfg_new_element(struct mCc_tac_element *tac_element,
+                        struct mCc_tac_cfg_element *next_element_left,
+                        struct mCc_tac_cfg_element *next_element_right);
+
+struct mCc_tac_cfg_element *mCc_tac_cfg_generate(struct mCc_tac_element *tac);
+
+struct mCc_tac_cfg_element *
+cfg_start_function(struct mCc_tac_element *tac_function_element);
+
+void cfg_connect_elements(struct mCc_tac_cfg_element *previous_element,
+                          struct mCc_tac_cfg_element *next_element_left,
+                          struct mCc_tac_cfg_element *next_element_right);
 
 #ifdef __cplusplus
 }
--- cc_team02/src/tac/cfg/tac_cfg.c
+++ cc_team02/src/tac/cfg/tac_cfg.c
@@ -7,6 +7,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-void mCc_tac_cfg_generate(struct mCc_tac_element *tac) {
+struct mCc_tac_cfg_element *
+mCc_tac_cfg_new_element(struct mCc_tac_element *tac_element,
+                        struct mCc_tac_cfg_element *next_element_left,
+                        struct mCc_tac_cfg_element *next_element_right)
+{
+	assert(tac_element);
+	assert(next_element_left);
 
+	struct mCc_tac_cfg_element *tac_cfg_element =
+	    malloc(sizeof(*tac_cfg_element));
+	if (!tac_cfg_element) {
+		return NULL;
+	}
+
+	tac_cfg_element->tac_element = tac_element;
+	tac_cfg_element->next_cfg_element_left = next_element_left;
+	tac_cfg_element->next_cfg_element_right = next_element_right;
+	return tac_cfg_element;
+}
+
+struct mCc_tac_cfg_element *mCc_tac_cfg_generate(struct mCc_tac_element *tac)
+{
+	// TODO iterate throw all function -> each function separate cfg
+	while (false) {
+	}
+	return NULL;
+}
+
+struct mCc_tac_cfg_element *
+cfg_start_function(struct mCc_tac_element *tac_function_element)
+{
+	// TODO build up cfg for each function here
+	return NULL;
+}
+
+void cfg_connect_elements(struct mCc_tac_cfg_element *previous_element,
+                          struct mCc_tac_cfg_element *next_element_left,
+                          struct mCc_tac_cfg_element *next_element_right)
+{
+	// TODO connect previous_elment with next elements
 }
