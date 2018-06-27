d748ab3293c532ab7641a8cd43bf23c2acace374
Juri Seelmann <juri.seelmann@student.uibk.ac.at>
2018-06-27 11:57:22 +0200

    improved cfg for tac

--- cc_team02/src/tac/cfg/tac_cfg.c
+++ cc_team02/src/tac/cfg/tac_cfg.c
@@ -13,7 +13,7 @@ mCc_tac_cfg_new_element(struct mCc_tac_element *tac_element,
                         struct mCc_tac_cfg_element *next_element_right)
 {
 	assert(tac_element);
-	assert(next_element_left);
+	// assert(next_element_left);
 
 	struct mCc_tac_cfg_element *tac_cfg_element =
 	    malloc(sizeof(*tac_cfg_element));
@@ -47,17 +47,36 @@ struct mCc_tac_cfg_element *
 cfg_start_function(struct mCc_tac_element *tac_function_element)
 {
 	struct mCc_tac_element *tac_next_element = tac_function_element;
+	struct mCc_tac_cfg_element *cfg_element;
+	struct mCc_tac_cfg_element *prev_cfg_element;
 	// TODO build up cfg for each function here
-	while (tac_next_element != MCC_TAC_OPARATION_END_FUNCTION_DEF) {
+	while (tac_next_element->tac_operation !=
+	       MCC_TAC_OPARATION_END_FUNCTION_DEF) {
+		prev_cfg_element = cfg_element;
+		cfg_element = mCc_tac_cfg_new_element(tac_next_element, NULL, NULL);
 
+		if (prev_cfg_element != NULL) {
+			if (prev_cfg_element->tac_element->tac_operation ==
+			        MCC_TAC_OPARATION_JUMP_NOT_EQUALS ||
+			    prev_cfg_element->tac_element->tac_operation ==
+			        MCC_TAC_OPARATION_JUMP ||
+			    prev_cfg_element->tac_element->tac_operation ==
+			        MCC_TAC_OPARATION_JUMP_EQUALS) {
+				// TODO where to jump? -> what is the right side?
+				prev_cfg_element->next_cfg_element_right = cfg_element;
+			}
+			prev_cfg_element->next_cfg_element_left = cfg_element;
+		}
 		tac_next_element = tac_next_element->tac_next_element;
 	}
 	return NULL;
 }
 
+// function not needed?
 void cfg_connect_elements(struct mCc_tac_cfg_element *previous_element,
                           struct mCc_tac_cfg_element *next_element_left,
                           struct mCc_tac_cfg_element *next_element_right)
 {
-	// TODO connect previous_elment with next elements
+	previous_element->next_cfg_element_left = next_element_left;
+	previous_element->next_cfg_element_right = next_element_right;
 }
