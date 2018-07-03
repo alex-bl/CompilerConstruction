1d345781112298373799777066a2e7d0f8f74f4d
Juri Seelmann <juri.seelmann@student.uibk.ac.at>
2018-07-03 15:37:43 +0200

    improved optic of tac cfg fixed basic test setup for tac cfg

--- cc_team02/src/tac/cfg/tac_cfg_print.c
+++ cc_team02/src/tac/cfg/tac_cfg_print.c
@@ -28,7 +28,7 @@ void tac_cfg_print_element(FILE *out, struct mCc_tac_cfg_element *cfg_element)
 		// print last edge/node of the right side
 		fprintf(out, "\t\"%p\" [shape=box, label=\"%s\"];\n", cfg_element,
 		        mCc_tac_print_op(cfg_element->tac_element->tac_operation));
-		fprintf(out, "\t\"%p\" -> \"%p\" [label=\"left_side\"];\n", cfg_element,
+		fprintf(out, "\t\"%p\" -- \"%p\" [label=\"\"];\n", cfg_element,
 		        cfg_element->next_cfg_element_left);
 	} else if (cfg_element->tac_element->tac_operation ==
 	               MCC_TAC_OPARATION_JUMP &&
@@ -37,7 +37,7 @@ void tac_cfg_print_element(FILE *out, struct mCc_tac_cfg_element *cfg_element)
 		// print last edge/node of the right side
 		fprintf(out, "\t\"%p\" [shape=box, label=\"%s\"];\n", cfg_element,
 		        mCc_tac_print_op(cfg_element->tac_element->tac_operation));
-		fprintf(out, "\t\"%p\" -> \"%p\" [label=\"left_side\"];\n", cfg_element,
+		fprintf(out, "\t\"%p\" -- \"%p\" [label=\"\"];\n", cfg_element,
 		        cfg_element->next_cfg_element_left);
 		fprintf(out, "\t\"%p\" [shape=box, label=\"%s\"];\n",
 		        cfg_element->next_cfg_element_left,
@@ -54,13 +54,13 @@ void tac_cfg_print_element(FILE *out, struct mCc_tac_cfg_element *cfg_element)
 			fprintf(out, "\t\"%p\" [shape=box, label=\"%s\"];\n", cfg_element,
 			        mCc_tac_print_op(cfg_element->tac_element->tac_operation));
 			// print left edge
-			fprintf(out, "\t\"%p\" -> \"%p\" [label=\"left_side\"];\n",
-			        cfg_element, cfg_element->next_cfg_element_left);
+			fprintf(out, "\t\"%p\" -- \"%p\" [label=\"\"];\n", cfg_element,
+			        cfg_element->next_cfg_element_left);
 
 			// print right edge if exists
 			if (cfg_element->next_cfg_element_right != NULL) {
-				fprintf(out, "\t\"%p\" -> \"%p\" [label=\"right_side\"];\n",
-				        cfg_element, cfg_element->next_cfg_element_right);
+				fprintf(out, "\t\"%p\" -- \"%p\" [label=\"\"];\n", cfg_element,
+				        cfg_element->next_cfg_element_right);
 				tac_cfg_print_element(out, cfg_element->next_cfg_element_right);
 			}
 			// cfg_element = cfg_element->next_cfg_element_left;
@@ -68,8 +68,8 @@ void tac_cfg_print_element(FILE *out, struct mCc_tac_cfg_element *cfg_element)
 			tac_cfg_print_element(out, cfg_element->next_cfg_element_left);
 		} else {
 			// print last element without further connections:
-			fprintf(out, "\t\"%p\" [shape=box, label=\"%d\"];\n", cfg_element,
-			        cfg_element->tac_element->tac_operation);
+			fprintf(out, "\t\"%p\" [shape=box, label=\"%s\"];\n", cfg_element,
+			        mCc_tac_print_op(cfg_element->tac_element->tac_operation));
 		}
 	}
 }
--- cc_team02/test/tac_cfg_print.cpp
+++ cc_team02/test/tac_cfg_print.cpp
@@ -3,16 +3,20 @@
 
 #include "gtest/gtest.h"
 
+#include "basic_tac.h"
 #include "config.h"
 #include "mCc/ast.h"
+#include "mCc/parser.h"
+#include "mCc/semantic_check.h"
 #include "mCc/tac.h"
-#include "mCc/tac_cfg_print.h"
 #include "mCc_test/mCc_test_utils.h"
+//#include "parser_helper.h"
+#include "mCc/tac_cfg_print.h"
 
 /*===========================================================================
  * program tests*/
 
-TEST(TacCfgPrintProgram, PrintCFGProgram)
+TEST(TacCfgPrintFunction, PrintCFGSimpleFunction)
 {
 
 	struct mCc_ast_function_def *function_def =
@@ -26,34 +30,42 @@ TEST(TacCfgPrintProgram, PrintCFGProgram)
 	struct mCc_tac_cfg_element *cfg = mCc_tac_cfg_generate(tac);
 
 	FILE *fp = open_file(CFG_DOT_TEST_OUTPUT_DIR, DOT_PREFIX,
-	                     "PrintCFGProgram", DOT_FILE_SUFFIX);
+	                     "PrintCFGSimpleFunction", DOT_FILE_SUFFIX);
+	mCc_tac_cfg_print(fp, cfg);
+	fclose(fp);
+}
+
+TEST(TacCfgPrintFunction, PrintCFGFunction)
+{
+	//======================== setup
+	const char *prog_to_parse =
+	    "void test(){int a;} void main(){int a; bool b;}";
+	struct mCc_parser_result result = mCc_parser_parse_string(prog_to_parse);
+
+	ASSERT_EQ(MCC_PARSER_TOP_LEVEL_PROGRAM, result.top_level_type);
+	ASSERT_TRUE(&(result.program) != NULL);
+	struct mCc_ast_program *prog = result.program;
+	int nr_of_semantic_errors = mCc_symtab_perform_semantic_checks(prog);
+	ASSERT_EQ(0, nr_of_semantic_errors);
+
+	struct mCc_tac_element *tac = mCc_tac_start_program(prog);
+	ASSERT_TRUE(tac != NULL);
+
+	struct mCc_tac_cfg_element *cfg = mCc_tac_cfg_generate(tac);
+	// mCc_assembly_calculate_stack_offsets(tac);
+	//======================== test
+
+	FILE *fp = open_file(CFG_DOT_TEST_OUTPUT_DIR, DOT_PREFIX,
+	                     "PrintCFGFunction", DOT_FILE_SUFFIX);
 	mCc_tac_cfg_print(fp, cfg);
 	fclose(fp);
 
-	// test_print_and_free_ast_program(program, "PrintProgramFunctionSimple");
-}
+	/*struct mCc_tac_identifier *param =
+	    get_tac_element_identifier(tac, 8, get_tac_result);
+	ASSERT_TRUE(param != NULL);
+	ASSERT_EQ(8, param->stack_offset);*/
 
-TEST(TacCfgPrintProgram, PrintProgramFunctionConcated)
-{
-	/*struct mCc_ast_function_def *function_def_1 =
-	    mCc_test_build_test_function_def(
-	        MCC_AST_DATA_TYPE_BOOL, "my_bool_function",
-	        mCc_test_build_test_lit_expression_bool(true));
-
-	struct mCc_ast_function_def *function_def_2 =
-	    mCc_test_build_test_function_def(MCC_AST_DATA_TYPE_VOID,
-	                                     "my_void_function", NULL);
-
-	struct mCc_ast_function_def *function_def_3 =
-	    mCc_test_build_test_function_def(
-	        MCC_AST_DATA_TYPE_INT, "my_int_function",
-	        mCc_test_build_test_lit_expression(10));
-
-	// concat
-	function_def_2->next_function_def = function_def_3;
-	function_def_1->next_function_def = function_def_2;
-
-	struct mCc_ast_program *program = mCc_ast_new_program(function_def_1);
-
-	test_print_and_free_ast_program(program, "PrintProgramFunctionConcated");*/
+	//======================== cleanup
+	mCc_ast_delete_program(prog);
+	mCc_tac_delete(tac);
 }
