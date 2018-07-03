10d7bb1beb195abad05f6dfaec920f3fc218b3e7
Juri Seelmann <juri.seelmann@student.uibk.ac.at>
2018-07-03 17:32:53 +0200

    fixed nested if structure for tac cfg

--- cc_team02/test/tac_cfg_print.cpp
+++ cc_team02/test/tac_cfg_print.cpp
@@ -176,7 +176,43 @@ TEST(TacCfgPrintFunction, PrintCFGIfElse)
 TEST(TacCfgPrintFunction, PrintCFGNestedIf)
 {
 	//======================== setup
-	const char *prog_to_parse = "void main(){int a; a=5; int b; b=5+a; if (a!=9) {if(b==3) {int c; c=0;}} }";
+	//const char *prog_to_parse = "void main(){int a; a=5; int b; b=5+a; if (a!=9) {/*if(b==3) {int c; c=0;}*/} }";
+	const char *prog_to_parse = "void main(){int a; a=5; if (a!=9) {a=a+1; if(a==8) {a=a+1;} } a=5;}";
+	struct mCc_parser_result result = mCc_parser_parse_string(prog_to_parse);
+
+	ASSERT_EQ(MCC_PARSER_TOP_LEVEL_PROGRAM, result.top_level_type);
+	ASSERT_TRUE(&(result.program) != NULL);
+	struct mCc_ast_program *prog = result.program;
+	int nr_of_semantic_errors = mCc_symtab_perform_semantic_checks(prog);
+	ASSERT_EQ(0, nr_of_semantic_errors);
+
+	struct mCc_tac_element *tac = mCc_tac_start_program(prog);
+	ASSERT_TRUE(tac != NULL);
+
+	struct mCc_tac_cfg_element *cfg = mCc_tac_cfg_generate(tac);
+	// mCc_assembly_calculate_stack_offsets(tac);
+	//======================== test
+
+	FILE *fp = open_file(CFG_DOT_TEST_OUTPUT_DIR, DOT_PREFIX,
+	                     "PrintCFGNestedIf", DOT_FILE_SUFFIX);
+	mCc_tac_cfg_print(fp, cfg);
+	fclose(fp);
+
+	/*struct mCc_tac_identifier *param =
+	    get_tac_element_identifier(tac, 8, get_tac_result);
+	ASSERT_TRUE(param != NULL);
+	ASSERT_EQ(8, param->stack_offset);*/
+
+	//======================== cleanup
+	mCc_ast_delete_program(prog);
+	mCc_tac_delete(tac);
+}
+
+TEST(TacCfgPrintFunction, PrintCFGNestedIfElse)
+{
+	//======================== setup
+	//const char *prog_to_parse = "void main(){int a; a=5; int b; b=5+a; if (a!=9) {/*if(b==3) {int c; c=0;}*/} }";
+	const char *prog_to_parse = "void main(){int a; a=5; if (a!=9) {if(a==8) {a=a+1;} } a=5;}";
 	struct mCc_parser_result result = mCc_parser_parse_string(prog_to_parse);
 
 	ASSERT_EQ(MCC_PARSER_TOP_LEVEL_PROGRAM, result.top_level_type);
